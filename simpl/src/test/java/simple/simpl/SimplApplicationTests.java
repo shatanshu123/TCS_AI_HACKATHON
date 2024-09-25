a38a70c54e70ae4261a48a061488a4778630e518
Shatanshu <shatanshu.1923csi1044@kiet.edu>
2024-09-25 22:49:51 +0530

    Backend Code

---
+++ simpl/src/test/java/simple/simpl/SimplApplicationTests.java
@@ -0,0 +1,13 @@
+package simple.simpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ workspace-spring-tool-suite-4-4.25.0.RELEASE/UserServiceImpl/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module UserServiceImpl {
+}
